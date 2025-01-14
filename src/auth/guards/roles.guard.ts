8e1be7778dcdb4fff5a1e7323feff175647105d5
sahatovbatyr <sahatovbatyr@gmail.com>
2025-01-14 21:56:16 +0300

    jwt guards added

---
+++ src/auth/guards/roles.guard.ts
@@ -0,0 +1,55 @@
+import { CanActivate, ExecutionContext, ForbiddenException, Injectable, UnauthorizedException } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { Reflector } from '@nestjs/core';
+import { ROLES_KEY } from '../../decorators/roles.decorator';
+import { JwtService } from '@nestjs/jwt';
+import { Role } from '../../role/entities/role.entity';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+
+  constructor( private reflector: Reflector,
+               private jwtService: JwtService ) {
+  }
+
+
+  async canActivate(context: ExecutionContext): Promise<boolean>  {
+
+    try {
+
+
+      const reqiredRoles = this.reflector.getAllAndOverride<string[]>(ROLES_KEY, [
+        context.getHandler(),
+        context.getClass
+      ]);
+
+      if ( !reqiredRoles) {
+        return true;
+      }
+
+      const req = context.switchToHttp().getRequest();
+      const authHeader = req.headers.authorization;
+
+      const bearer = authHeader.split(" ")[0];
+      const token = authHeader.split(" ")[1];
+
+      if ( bearer!=="Bearer" || !token) {
+        throw new UnauthorizedException("Unauthorized user");
+      }
+
+      const payload = await this.jwtService.verifyAsync(token, {secret: "SECRET_KEY"});
+      req.user = {id: payload.userId, username: payload.username, roles: payload.roles};
+
+      return req.user.roles.some( (role: Role) => reqiredRoles.includes(role.title) );
+
+
+    } catch (err ) {
+
+      throw new ForbiddenException("Access DENIED:"+err);
+
+    }
+
+  }
+
+
+}
