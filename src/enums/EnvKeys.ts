f5042d2eb4facfd66a4312ce6df495a4fd393b0d
sahatovbatyr <sahatovbatyr@gmail.com>
2025-01-26 18:46:23 +0300

    moved AppEnv, AppEnvInterface to a different file

---
+++ src/enums/EnvKeys.ts
@@ -0,0 +1,10 @@
+export enum EnvKeys {
+  HOST = 'HOST',
+  HOST_PORT = 'HOST_PORT',
+  DB_HOST = 'DB_HOST',
+  DB_PORT = 'DB_PORT',
+  DB_USER = 'DB_USER',
+  DB_NAME = 'DB_NAME',
+  DB_PASSWORD = 'DB_PASSWORD'
+
+}
